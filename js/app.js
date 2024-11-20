eec781082659c4b69332093af3fedec678050a7d
Márton Lente <lentemarton@gmail.com>
2024-11-20 13:33:47 +0100

    Fix add js app function imgsLoaded to set isotopeGridHelper height

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 (function() {
+  function imgsLoaded(container, callback) {
+    var imgs = container.getElementsByTagName('img');
+    var loadedImgsCount = 0;
+    var totalImgsCount = imgs.length;
+
+    if (totalImgsCount == 0) {
+      callback();
+
+      return;
+    }
+
+    for (var i = 0; i < totalImgsCount; i++) {
+      var img = imgs[i];
+
+      img.addEventListener('load', checkImage);
+      img.addEventListener('error', checkImage);
+    }
+
+    function checkImage() {
+      loadedImgsCount++;
+
+      if (loadedImgsCount == totalImgsCount) {
+        callback();
+      }
+    }
+  }
+
   function isotopeGrid() {
     var isotopeGrid = document.querySelector('.js-isotope-grid');
 
@@ -9,7 +36,12 @@
   }
 
   function init() {
-    isotopeGrid();
+    // TODO: check variable naming
+    var isotopeGridHelper = document.querySelector('.js-isotope-grid');
+
+    imgsLoaded(isotopeGridHelper, function() {
+      isotopeGrid();
+    });
   }
 
   document.addEventListener('DOMContentLoaded', function() {
