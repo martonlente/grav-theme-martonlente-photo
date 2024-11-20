0ab29e29e10824438a61a5861af456963b844995
Márton Lente <lentemarton@gmail.com>
2024-11-20 13:24:55 +0100

    Add js app functions init and isotopeGrid base

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 (function() {
-  // TODO: remove
-  console.log('Hello World!');
+  function isotopeGrid() {
+    var isotopeGrid = document.querySelector('.js-isotope-grid');
+
+    var iso = new Isotope(isotopeGrid, {
+      itemSelector: '.js-isotope-grid-item',
+      layoutMode: 'fitRows'
+    });  
+  }
+
+  function init() {
+    isotopeGrid();
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    init();
+  });
 })();
